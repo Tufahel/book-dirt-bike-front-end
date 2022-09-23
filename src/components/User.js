9c372bfeab3569f931711b4fe7f80639dc97f207
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-23 09:28:35 +0100

    Fix user infinte loop

--- src/components/User.js
+++ src/components/User.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../redux/actions/User';
 
 export default function User() {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.UsersReducer);
-  console.log(users);
-  dispatch(getUsers());
+  // console.log(users);
+  // dispatch(getUsers());
 
+  useEffect(() => {
+    dispatch(getUsers());
+  }, []);
   return (
     <>
-      {users.value.map((user) => (
+      {users.value?.map((user) => (
         <p key={user.id}>
-          user name
-          {' '}
+          Welcome: &nbsp;
           {user.name}
+          ,
           {' '}
-          id
+          Your User id is &nbsp;
           {user.id}
           .
         </p>
