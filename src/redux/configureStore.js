9973e46db6efae4144530ebb3b4d9b7c1253b89b
Nii Amanor Djoleto <djoletoamanor@gmail.com>
2022-09-24 13:12:50 +0000

    Update Store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,12 +5,14 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { UsersReducer, SignupReducer, SigninReducer } from './reducers/User';
 import MotorcyclesReducer from './reducers/Motorcycle';
+import RentalsReducer from './reducers/Rentals';
 
 const rootReducer = combineReducers({
   SignupReducer,
   SigninReducer,
   UsersReducer,
   MotorcyclesReducer,
+  RentalsReducer,
 });
 const store = createStore(rootReducer,
   compose(applyMiddleware(thunk, logger)));
