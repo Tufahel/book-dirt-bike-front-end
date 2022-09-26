0d07efa47063936cff152fd051e57ef219469bbb
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-26 11:06:56 +0100

    Refactor store

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
-import User from './components/User';
+// import { Provider } from 'react-redux';
+// import store from './redux/configureStore';
+// import User from './components/User';
 // import Motorcycle from './components/Motorcycle';
 import Motorcycle from './components/motorcycle/Motorcycle';
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import Navigation from './components/Navigation/Navigation';
+import AddMotorcycle from './components/motorcycle/AddMotorcycle';
 
 const App = () => (
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/nav" element={<Navigation />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/motorcycle" element={<Motorcycle />} />
-        </Routes>
-      </Router>
-    </Provider>
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Navigation />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/" element={<User />} /> */}
+      <Route path="/motorcycle" element={<Motorcycle />} />
+      <Route path="/addmotorcycle" element={<AddMotorcycle />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
