aa463f1ae2d36e25deb85780194c812439bfff2e
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-27 01:01:38 +0100

    Fix navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Navigation.css';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signOut } from '../../redux/actions/User';
@@ -14,23 +15,34 @@ function Navigation() {
 
   return (
     <>
-      <div className="">
-        logo
-      </div>
-      <div className="">
-        <div className="">
-          <ul className="">
-            <li className="" key="link1">
-              <NavLink to="/motorcycle" className="">Bikes</NavLink>
-            </li>
-            {
+      <nav className="sidenav">
+        <div className="divider">
+          <div className="">
+            logo
+          </div>
+          <div className="">
+            <div className="">
+              <ul className="nav-link">
+                <li className="">
+                  <NavLink
+                    className="link"
+                    to="/"
+                    exact
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="" key="link1">
+                  <NavLink to="/motorcycle" className="link">Bikes</NavLink>
+                </li>
+                {
               user && (
                 <>
                   <li className="" key="link2">
-                    <NavLink to="/motorcycle" className="">Add Bike</NavLink>
+                    <NavLink to="/motorcycle" className="link">Add Bike</NavLink>
                   </li>
                   <li className="" key="link3">
-                    <NavLink to="/rentals" className="">Reservations</NavLink>
+                    <NavLink to="/rentals" className="link">Reservations</NavLink>
                   </li>
                   <li className="" key="logout_link">
                     <button
@@ -44,39 +56,41 @@ function Navigation() {
                 </>
               )
             }
-            {
+                {
               (user === null) && (
                 <>
                   <li className="" key="signin_link">
-                    <NavLink to="/login" className="">Login</NavLink>
+                    <NavLink to="/login" className="link">Login</NavLink>
                   </li>
                   <li className="" key="signup_link">
-                    <NavLink to="/signup" className="">Registration</NavLink>
+                    <NavLink to="/signup" className="link">Registration</NavLink>
                   </li>
                 </>
               )
             }
-          </ul>
+              </ul>
+            </div>
+          </div>
+          <div className="social">
+            <div className="">
+              <ul className="">
+                <li>
+                  Facebook
+                </li>
+                <li>
+                  Email
+                </li>
+                <li>
+                  Call
+                </li>
+              </ul>
+              <footer>
+                <p>&#169; 2020 All right Reserved</p>
+              </footer>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="">
-        <div className="">
-          <ul className="">
-            <li>
-              Facebook
-            </li>
-            <li>
-              Email
-            </li>
-            <li>
-              Call
-            </li>
-          </ul>
-          <footer>
-            <p>&#169; 2020 All right Reserved</p>
-          </footer>
-        </div>
-      </div>
+      </nav>
     </>
   );
 }
